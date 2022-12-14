b1c459f0acacf6b83eb223ba42c812df63524a0d
K-Lab <utt.klab@gmail.com>
2022-12-14 16:00:00 -0700

    pretest now contains 17 questions

--- pretest.py
+++ pretest.py
@@ -33,7 +33,7 @@ def run_q3():
     return q3
 
 def run_q4():
-    q4_prompt = "Question 4: What is a SNP?\n"
+    q4_prompt = "Question 4: What is a SNP? (hint: this is a biology question, not a computer science question)\n"
     q4 = get_response(q4_prompt)
     return q4
 
@@ -63,13 +63,49 @@ def run_q9():
     return q9
 
 def run_q10():
-    q10_prompt = "Quesetion 10: Is there anything in particular you want to learn before you graduate?\n"
+    q10_prompt = "Question 10: Is there anything in particular you want to learn before you graduate?\n"
     q10 = get_response(q10_prompt)
     return q10
 
+def run_q11():
+    q11_prompt = "Question 11: On a scale of 0-9, how comfortable are you using the command line?\n"
+    q11 = get_response(q11_prompt)
+    return q11
+
+def run_q12():
+    q12_prompt = "Question 12: On a scale of 0-9, how comfortable are you coding in bash?\n"
+    q12 = get_response(q12_prompt)
+    return q12
+
+def run_q13():
+    q13_prompt = "Question 13: On a scale of 0-9, how comfortable are you coding in python?\n"
+    q13 = get_response(q13_prompt)
+    return q13
+
+def run_q14():
+    q14_prompt = "Question 14: In computer science, what is a variable?\n"
+    q14 = get_response(q14_prompt)
+    return q14
+
+def run_q15():
+    q15_prompt = "Question 15: In computer science, what is a function?\n"
+    q15 = get_response(q15_prompt)
+    return q15
+
+def run_q16():
+    q16_prompt = "Question 16: What is a for loop? What is a while loop?\n"
+    q16 = get_response(q16_prompt)
+    return q16
+
+def run_q17():
+    q17_prompt = "Question 17: What is parallelization? Is running commands in-parallel more efficient than running them in a loop?\n"
+    q17 = get_response(q17_prompt)
+    return q17
+
+
 if __name__ == "__main__":
     with open("test_results.txt", 'w') as outfile:
-        for i in range(0,11):
+        for i in range(0,18):
             run_code = "run_q" + str(i) + "()"
             outfile.write(eval(run_code))
 
